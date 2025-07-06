6d9c4976367399a22537979c9fb56ae25f5d6a01
sanxroz <san7yx@gmail.com>
2025-07-05 20:41:51 -0700

    removed  useCallback

--- src/components/ProjectTree.tsx
+++ src/components/ProjectTree.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useCallback } from "react";
+import { useState } from "react";
 import { TreeNode as TreeNodeType } from "@/lib/tree-builder";
 import { useTreeExpansion } from "@/hooks/useTreeExpansion";
 import { TreeControls } from "./tree/TreeControls";
