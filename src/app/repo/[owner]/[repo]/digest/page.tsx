5344f02728d95be57ce92cc87ca0e026bd05c196
sanxroz <san7yx@gmail.com>
2025-07-06 17:21:20 -0700

    clean a padding

--- src/app/repo/[owner]/[repo]/digest/page.tsx
+++ src/app/repo/[owner]/[repo]/digest/page.tsx
@@ -236,7 +236,7 @@ export default function DigestPage() {
         </div>
       </header>
 
-      <div className="max-w-7xl mx-auto px-4 py-8">
+      <div className="max-w-7xl mx-auto px-4 py-4">
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
           {/* Insights Sidebar */}
           {metadata && (
